f39d3576484cf30a591a76cd2c69b114e51e9787
Kagome Miller <kagomemiller@gmail.com>
2021-02-13 21:39:44 -0600

    this one I need help with. will get back to.

---
+++ range7.py
@@ -0,0 +1,15 @@
+school = Massachusetts Institute Of Technology
+numV = 0
+numC = 0
+
+for char in school:
+    if char == 'o' or char == 'e' or char == 'i' \
+    or char == 'a' or char == 'u'
+        numV += 1
+    elif char == 'O' or char == "M"
+        print(char)
+    else:
+        numC -= 1
+
+print('numV is: ' + str(numV))
+print('numC is: ' + str(numC))
